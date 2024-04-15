5949cd17662ba0fe7efb6cbecdba1b243cd7098e
Diana Stoica <diana.stoica@devplant.ro>
2024-04-15 14:54:13 +0300

    added the package manager to projects

--- core/constants.ts
+++ core/constants.ts
@@ -1,2 +1,37 @@
 export const OUTDATED_MONTHS = 12
 export const OUT_OF_SUPPORT_MONTHS = 24
+
+export const extractorFiles: Map<string, string[]> = new Map<string, string[]>(
+    [
+        ['java', ['pom.xml', 'build.gradle', 'build.gradle.kts', 'deptree.txt']],
+        ['dotnet', ['*.csproj', '*.fsproj', '*.vbproj']],
+        ['javascript', ['package.json', 'package-lock.json', 'yarn.lock']],
+        ['php', ['composer.json', 'composer.lock']],
+        ['python', ['requirements.txt', 'setup.py', 'Pipfile', 'Pipfile.lock', 'pyproject.toml', 'poetry.lock']],
+        ['ruby', ['Gemfile', '*.gemspec', 'Gemfile.lock']],
+    ]
+)
+
+export const fileToPackageManager: Map<string, string> = new Map<string, string>([
+        ['pom.xml', 'Maven'],
+        ['deptree.txt', 'Maven'],
+        ['build.gradle', 'Gradle'],
+        ['build.gradle.kts', 'Gradle'],
+        ['*.csproj', 'NuGet'],
+        ['*.fsproj', 'NuGet'],
+        ['*.vbproj', 'NuGet'],
+        ['package.json', 'npm'],
+        ['package-lock.json', 'npm'],
+        ['yarn.lock', 'Yarn'],
+        ['composer.json', 'Composer'],
+        ['composer.lock', 'Composer'],
+        ['requirements.txt', 'pip'],
+        ['setup.py', 'setuptools'],
+        ['Pipfile', 'pipenv'],
+        ['Pipfile.lock', 'pipenv'],
+        ['pyproject.toml', 'Poetry'],
+        ['poetry.lock', 'Poetry'],
+        ['Gemfile', 'Bundler'],
+        ['*.gemspec', 'RubyGems'],
+        ['Gemfile.lock', 'Bundler']
+])
--- depinder-fe/src/app/common/standalone/projects-table/projects-table.component.ts
+++ depinder-fe/src/app/common/standalone/projects-table/projects-table.component.ts
@@ -3,23 +3,62 @@ import { CommonModule } from '@angular/common';
 import {MatTableModule} from "@angular/material/table";
 import {Router} from "@angular/router";
 import { Project } from '@core/project';
+import {extractorFiles, fileToPackageManager} from "@core/constants";
 
 @Component({
   selector: 'app-projects-table',
+  //todo remove standalone
   standalone: true,
-    imports: [CommonModule, MatTableModule],
+  imports: [CommonModule, MatTableModule],
   templateUrl: './projects-table.component.html',
   styleUrl: './projects-table.component.css'
 })
 export class ProjectsTableComponent {
-
-  constructor(private router: Router,) {
-  }
-
   @Input() projects: Project[] = [];
-  displayedColumns: string[] = ['name', 'projectPath'];
+  displayedColumns: string[] = ['name',  'project-type', 'package-manager', 'manifest-file', 'lock-file', 'projectPath',];
+
+  constructor(private router: Router) { }
 
   navigate(projectId: string) {
     this.router.navigate(['/project', projectId]);
+  }
+
+  getFile(filePath?: string) {
+    if (!filePath) {
+      return undefined;
+    }
+    return filePath.split('/').pop();
+  }
+
+  getProjectType(project: Project) {
+    const fileName = this.getFile(project.manifestFile ?? project.lockFile);
+    const fileExtension = fileName?.split('.').pop();
+
+    if (fileName) {
+      for (let [key, fileNames] of extractorFiles.entries()) {
+        if (fileNames.includes(fileName) || fileNames.some(name => name === `*.${fileExtension}`)) {
+          return key;
+        }
+      }
+    }
+    return undefined;
+  }
+
+  determinePackageManager(project: Project): string | undefined {
+    const fileName = this.getFile(project.lockFile ?? project.manifestFile);
+
+    if (fileName !== undefined) {
+      for (let [filePattern, manager] of fileToPackageManager.entries()) {
+        if (filePattern.includes('*')) {
+          const regex = new RegExp(filePattern.replace('*', '.*') + '$');
+          if (regex.test(fileName)) {
+            return manager;
+          }
+        } else if (filePattern === fileName) {
+          return manager;
+        }
+      }
+    }
+    return undefined;
   }
 }
