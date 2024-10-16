5fbfdcb1db1acaaeb750f4b734f4ffaf5481383a
AvrAlexandra <avramescu.alexandra@gmal.com>
2024-10-16 18:27:45 +0300

    Added method to retrieve changed files for each commit

--- src/commands/history/history.ts
+++ src/commands/history/history.ts
@@ -1,10 +1,10 @@
 import { Command } from 'commander'
 import git from 'isomorphic-git'
 import fs from 'fs'
-import { AnalyseOptions } from '../analyse'
-import { getPluginsFromNames } from '../../plugins'
-import { DepinderProject } from "../../extension-points/extract";
-import { Plugin } from "../../extension-points/plugin";
+import {AnalyseOptions} from '../analyse'
+import {getPluginsFromNames} from '../../plugins'
+import {DepinderProject} from "../../extension-points/extract"
+import {Plugin} from "../../extension-points/plugin"
 
 export const historyCommand = new Command()
     .name('history')
@@ -37,6 +37,44 @@ export async function analyseHistory(folders: string[], options: AnalyseOptions,
 // Function to process each commit and update the map by plugin
 async function processCommitForPlugins(commit: any, folder: string, selectedPlugins: Plugin[], projectMap: Map<string, { commit: string, projects: DepinderProject[] }[]>) {
     console.log('Commit:', JSON.stringify(commit, null, 2));
+    const changes = await getChangedFiles(commit, folder);
+    console.log("Changes: ", changes);
+}
+
+async function getChangedFiles(commit: any, folder: string): Promise<string[]> {
+    const changedFiles: string[] = [];
+
+    const currentCommitOid = commit.oid;
+    const parentCommitOid = commit.commit.parent[0];
+
+    try {
+        const diff = await git.walk({
+            fs,
+            dir: folder,
+            trees: [git.TREE({ ref: currentCommitOid }), git.TREE({ ref: parentCommitOid })],
+            map: async function (filepath, [head, base]) {
+                if (!head && base) {
+                    // File was deleted
+                    changedFiles.push(filepath);
+                } else if (head && !base) {
+                    // File was added
+                    changedFiles.push(filepath);
+                } else if (head && base) {
+                    // File was modified
+                    const headOid = await head.oid();
+                    const baseOid = await base.oid();
+                    if (headOid !== baseOid) {
+                        changedFiles.push(filepath);
+                    }
+                }
+            }
+        });
+
+    } catch (error) {
+        console.error(`Error getting changed files for commit ${currentCommitOid}:`, error);
+    }
+
+    return changedFiles;
 }
 
 // Function to fetch commits from a Git repository
